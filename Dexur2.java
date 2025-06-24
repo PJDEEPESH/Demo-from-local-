ffb52526d559ec0e3c080ccead43383b6d7cf4bd
PJDEEPESH <deepeshp.j.2004@gmail.com>
2025-06-24 10:49:29 +0530

    files added

---
+++ Dexur2.java
@@ -0,0 +1,25 @@
+class Dexur
+{
+    public static void main(String[] args) 
+    {
+        int[] arr1 = {1,2,3,1};
+        int[] arr2 = {2,7,9,3,1};
+        int[] arr3 = {0,0,0,0};
+        int[] arr4 = {10};
+        int[] arr5 = {10,5,20,100,10,5};
+        int[] arr6 = {5,5,10,100,10,5};
+        MaximumMoney(arr1);
+        // MaximumMoney(arr2);
+        // MaximumMoney(arr3);
+        // MaximumMoney(arr4);
+        // MaximumMoney(arr5);
+        // MaximumMoney(arr6);
+    }
+    public static void MaximumMoney(int[] nums)
+    {
+        for(int i = 0;i<nums.length;i++)
+        {
+            
+        }
+    }
+}
